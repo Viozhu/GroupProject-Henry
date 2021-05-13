a953465e0d52ba14e5bbbea0e5b50fe5fc0ac395
jorge ignacio garay <nachog_man@icloud.com>
2021-05-13 15:55:56 -0300

    config

--- api/src/db/config/db.config.js
+++ api/src/db/config/db.config.js
@@ -4,23 +4,37 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.database_development,
+    database: process.env.DB_NAME_DEVELOPMENT,
     host: process.env.HOST_NAME,
     dialect: 'postgres',
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.database_test,
+    database: process.env.DB_NAME_TEST,
     host: process.env.HOST_NAME,
     dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.database_production,
+    database: process.env.DB_NAME_PRODUCTION,
     host: process.env.HOST_NAME,
     dialect: 'postgres',
     logging: false,
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000,
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        // Ref.: https://github.com/brianc/node-postgres/issues/2009
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+    ssl: true,
   },
 };
